84415e2c02446921d408fc588dc65d15ca198b52
Joshua Matthews <jmatth93@gmail.com>
2025-01-25 13:23:10 -0600

    added comments

--- example/c/main.c
+++ example/c/main.c
@@ -5,12 +5,14 @@
 #include <sys/cdefs.h>
 #include "funnel.h"
 
+// simple example structure
+// we pack it to calculate size easier
 struct __attribute__((packed)) data {
   uint8_t age;
   char grade;
 };
 
-
+// Define our marshal method for the writer.
 int data_marshal(void* obj, uint8_t* buffer, size_t len) {
   struct data *local = (struct data*)obj;
   printf("marshal data: age=%d, grade=%c\n", local->age, local->grade);
@@ -19,6 +21,7 @@ int data_marshal(void* obj, uint8_t* buffer, size_t len) {
   return 0;
 }
 
+// Define our unmarshal method for the reader
 void* data_unmarshal(uint8_t* buffer) {
   struct data *local = malloc(sizeof(struct data));
   local->age = buffer[0];
@@ -26,10 +29,12 @@ void* data_unmarshal(uint8_t* buffer) {
   return local;
 }
 
+// Define our method to return the size of our data after marshalled.
 size_t data_size() {
   return sizeof(struct data);
 }
 
+// Define our callback to be called within the reader
 void callback(void* obj) {
   struct data *local = (struct data*)obj;
   printf("read data: age=%d, grade=%c\n", local->age, local->grade);
@@ -37,42 +42,47 @@ void callback(void* obj) {
 }
 
 int main(int argc, char **argv) {
+  // create a funnel structure
   struct funnel_t fun;
+  // create our event marshaller with our defined functions
   struct event_marshaller_t marshaller = {
     .marshal = data_marshal,
     .unmarshal = data_unmarshal,
     .size = data_size,
   };
+  // initialize
   int ret = funnel_init(&fun, marshaller);
   if (ret != 0) {
     printf("funnel failed to initialize: %d\n", ret);
     exit(1);
   }
 
+  // create our data and event
   struct data obj = {
    .age = 31,
    .grade = 'A',
   };
-
   struct event_t e = {
     .payload = &obj,
   };
 
+  // just to show our original values
   printf("init data: age=%d, grade=%c\n", obj.age, obj.grade);
 
+  // write data
   struct funnel_result result = funnel_write(&fun, e);
-
   if (result.result != SUCCESS) {
     printf("funnel write failed: %d\n", result.result);
     exit(1);
   }
 
+  // read data with our callback
   result = funnel_read(&fun, callback);
-
   if (result.result != SUCCESS) {
     printf("funnel read failed: %d\n", result.result);
     exit(1);
   }
 
+  // free funnel internals
   funnel_free(&fun);
 }
